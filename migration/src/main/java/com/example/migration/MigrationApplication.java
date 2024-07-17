3b2237e389289f27055df383e8861853a69b6ba6
Ali Zaarour <zaarour.a@outlook.com>
2024-07-17 21:14:32 +0300

    init project migration

---
+++ migration/src/main/java/com/example/migration/MigrationApplication.java
@@ -0,0 +1,14 @@
+package com.example.migration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationApplication {
+
+	public static void main(String[] args) {
+		var applicationContext =  SpringApplication.run(MigrationApplication.class, args);
+		applicationContext.close();
+	}
+
+}
